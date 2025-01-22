b090324d40efad1402d882986662d4175743c04f
ere_vg <Eremin@spasskievorota.com>
2025-01-22 16:03:28 +0300

    no message

---
+++ CMS_GRID/list.php
@@ -0,0 +1,67 @@
+<?php
+
+if (isset($_POST['id'])) {
+
+include_once 'grid.php';
+
+$gr = new grid;
+
+$gr->id = 'id_list_1';
+
+$gr->info = [
+    [
+        "type" => "table",
+        "name" => "table_1",
+        "syn" => "t1",
+        "id_field_syn" => "id1",
+        "fields" => [
+            [
+                "name" => "id",
+                "syn" => "id1",
+                "type" => "int",
+                "visible" => "no"
+            ],            
+            [
+                "name" => "name",
+                "syn" => "n1",
+                "type" => "string",
+                "№" => 1,                  
+                "caption" => "Название"
+            ]
+        ]
+    ],  
+    [
+        "type"=>"hidden",
+        "id"=>"grid_list_id"
+    ],   
+    [
+        "type"=>"hidden",
+        "id"=>"grid_list_name"
+    ],               
+    [
+        "type"=>"hidden",
+        "id"=>"grid_list_action"
+    ],       
+    [
+        "type"=>"button",
+        "text"=>"Выбрать",
+        "class"=>"select",
+    ],
+    [
+        "type"=>"button",
+        "text"=>"Отмена",
+        "class"=>"cancel"
+    ],
+    [
+        "type"=>"button",
+        "text"=>"Очистить",
+        "class"=>"clear"
+    ]
+];
+
+echo $gr->show();
+
+}
+
+?>
+
