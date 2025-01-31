3f79b7260f8d74aa605bcf320952a40d2a51a59e
ere_vg <Eremin@spasskievorota.com>
2025-01-31 17:23:44 +0300

    no message

--- CMS_GRID/file.php
+++ CMS_GRID/file.php
@@ -5,7 +5,7 @@ if (isset($_POST['grid_id'])) {
     echo '<div id=file_'.$_POST['grid_id'].' class=file_class done=N path="'.$_POST['path'].'">'
             . '<input type=file id=file_'.$_POST['grid_id'].'_'.$_POST['name'].' class=grid_file>'
             . '<button class=file_select>Выбрать</button>'
-            . '<button class=file_save>Сохранить</button>'
+            . '<button class=file_save>Загрузить</button>'
              . '<button class=file_clear>Очистить</button>'
             . '<button class=file_cancel>Закрыть</button>'
             . '</div>';
