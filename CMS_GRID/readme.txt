6a89262ed553fac19db7e4b7d11b8b943d2ac4e9
ere_vg <Eremin@spasskievorota.com>
2025-03-06 11:31:50 +0300

    no message

--- CMS_GRID/readme.txt
+++
@@ -1,9 +0,0 @@
-
-Samples:
-
-sample_1.php - simple data list 
-sample_1.sql - sql script for simple data list 
-
-
-sample_2.php - simple data tree 
-sample_2.sql - sql script for simple data tree
--- CMS_GRID/tables.sql
+++ CMS_GRID/tables.sql
@@ -5,8 +5,10 @@ CREATE TABLE `table1` (
   `selectid` int DEFAULT NULL COMMENT 'Ссылка на список',
   `listid` int DEFAULT NULL COMMENT 'Ссылка на справочник',
   `discr` varchar(45) CHARACTER SET cp1251 COLLATE cp1251_bin DEFAULT NULL,
+  `chk` char(1) COLLATE cp1251_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=cp1251 COLLATE=cp1251_bin COMMENT='test table1'
+) ENGINE=InnoDB AUTO_INCREMENT=291 DEFAULT CHARSET=cp1251 COLLATE=cp1251_bin COMMENT='test table1'
+
 
 
 CREATE TABLE `table2` (
@@ -14,5 +16,22 @@ CREATE TABLE `table2` (
   `name` varchar(45) COLLATE cp1251_bin DEFAULT NULL,
   `caption` text COLLATE cp1251_bin,
   `fname` varchar(255) COLLATE cp1251_bin DEFAULT NULL,
+  `parentid` int DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=cp1251 COLLATE=cp1251_bin COMMENT='Test Table2'
+
+
+
+insert into `table1` (  `id`,  `date`,  `sum`,  `selectid`,  `listid`,  `discr`,  `chk`)
+values (1, '2025-01-30', 11.1, 1, null, '1', 'N');
+insert into `table1` (  `id`,  `date`,  `sum`,  `selectid`,  `listid`,  `discr`,  `chk`)
+values (2, '2025-01-30', 11.1, 1, null, '1', 'N');
+
+
+
+insert into `table2` ( `id`,  `name`,  `caption`,  `fname`,  `parentid`)
+values(1, 'name 1', 'caption 1', null, null);
+insert into `table2` ( `id`,  `name`,  `caption`,  `fname`,  `parentid`)
+values(2, 'name 1', 'caption 1', null, 1);
+
+
